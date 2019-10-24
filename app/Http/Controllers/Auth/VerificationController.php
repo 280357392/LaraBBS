210478e4968a28c4c5d858e639f69e4115cd4c77
Meng Wei <yyy333567@163.com>
2019-10-24 18:15:01 +0000

    认证后的消息提醒

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 
+//此控制器处理所有邮件认证相关逻辑
 class VerificationController extends Controller
 {
     /*
@@ -29,13 +30,17 @@ class VerificationController extends Controller
 
     /**
      * Create a new controller instance.
+     * 构建函数里使用了三个中间件
      *
      * @return void
      */
     public function __construct()
     {
+        //设定了所有的控制器动作都需要登录后才能访问
         $this->middleware('auth');
+        //只有 verify 动作使用 signed 中间件进行认证
         $this->middleware('signed')->only('verify');
+        //对 verify 和 resend 动作做了频率限制，限定了这两个动作访问频率是 1 分钟内不能超过 6 次
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
