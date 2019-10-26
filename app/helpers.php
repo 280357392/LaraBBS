2205cd7152eab4427f0f3a475abda5b80c1fe1af
Meng Wei <yyy333567@163.com>
2019-10-26 14:38:46 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -10,3 +10,21 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+
+/**
+ * {{ category_nav_active(1) }}
+ * active_class 的用法
+ * 如果 $condition 不为 False 即会返回字符串 `active`
+ */
+function category_nav_active($category_id)
+{
+    return active_class(
+        (
+            //当前访问的URL是：http://larabbs.test/categories/{category_id}  这个方法就返回 "active"
+            //if_route () - 判断当前对应的路由是否是指定的路由；
+            //if_route_param () - 判断当前的 url 有无指定的路由参数。
+            if_route('categories.show') && if_route_param('category', $category_id)
+        )
+    );
+}
