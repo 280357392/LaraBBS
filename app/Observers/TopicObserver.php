27306fd32f4b6bb4b173c1988cf787bf0b154ec6
Meng Wei <yyy333567@163.com>
2019-10-28 11:50:15 +0000

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -31,6 +31,7 @@ class TopicObserver
     //观察者类里的方法名对应 Eloquent 想监听的事件
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
         //make_excerpt() 是我们自定义的辅助方法，我们需要在 helpers.php 文件中添加
         $topic->excerpt = make_excerpt($topic->body);
     }
