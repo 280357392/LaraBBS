de0c47da6f944380a14d58a72c9d493ed19ad5ee
Meng Wei <yyy333567@163.com>
2019-10-28 17:21:19 +0000

    用户可以删除自己发的话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -79,7 +79,6 @@ class TopicsController extends Controller
     {
         $this->authorize('destroy', $topic);
         $topic->delete();
-
         return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
     }
 
