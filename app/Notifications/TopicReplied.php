14bb46577fa7f6fab66621f21f17582edc446c85
Meng Wei <yyy333567@163.com>
2019-10-29 11:55:01 +0000

    支持队列的邮件通知(未加入队列)

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -20,12 +20,6 @@ class TopicReplied extends Notification
         $this->reply = $reply;
     }
 
-    public function via($notifiable)
-    {
-        // 开启通知的频道
-        return ['database'];
-    }
-
     public function toDatabase($notifiable)
     {
         $topic = $this->reply->topic;
@@ -43,4 +37,20 @@ class TopicReplied extends Notification
             'topic_title' => $topic->title,
         ];
     }
+
+    public function via($notifiable)
+    {
+        // 开启通知的频道
+        return ['database', 'mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+            ->line('你的话题有新回复！')
+            ->action('查看回复', $url);
+    }
+
 }
