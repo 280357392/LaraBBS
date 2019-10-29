a2a541903559627c4451e54a80f1d518793f4936
Meng Wei <yyy333567@163.com>
2019-10-29 11:17:29 +0000

    回复列表

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -67,6 +67,17 @@
 
                 </div>
             </div>
+
+            {{-- 用户回复列表 --}}
+            <div class="card topic-reply mt-4">
+                <div class="card-body">
+                    {{-- 回复框--}}
+                    @include('topics._reply_box', ['topic' => $topic])
+                    {{--用户回复列表--}}
+                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+                </div>
+            </div>
+
         </div>
     </div>
 @stop
