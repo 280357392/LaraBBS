e6904b96a373c539324fa6fe210d00027f03bde1
Meng Wei <yyy333567@163.com>
2019-10-29 11:30:56 +0000

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,22 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
-    {
-        //
-    }
 
     public function updating(Reply $reply)
     {
         //
+    }
+
+    //话题下每新增一条回复+!
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+    }
+
+    //解决 XSS 安全威胁
+    public function creating(Reply $reply)
+    {
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -71,12 +71,11 @@
             {{-- 用户回复列表 --}}
             <div class="card topic-reply mt-4">
                 <div class="card-body">
-                    {{-- 回复框--}}
-                    @include('topics._reply_box', ['topic' => $topic])
-                    {{--用户回复列表--}}
+                    @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
                     @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
                 </div>
             </div>
+            .
 
         </div>
     </div>
